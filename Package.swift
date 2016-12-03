742fecc35dbb6909ba095623c8f4cc978d1054e9
Travis Palmer <palmer.travis@gmail.com>
2016-12-03 16:43:31 -0500

    initial commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "vapor-api",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+    ]
+)
